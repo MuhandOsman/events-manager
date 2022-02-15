6f757ca33678e61862c1b295747c3334258f7006
omaralezzi <81753019+omaralezzi@users.noreply.github.com>
2022-02-15 16:25:53 +0100

    added the hashed user password method
    we need to test it with the database before and after hashing password

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,25 +1,29 @@
-const User = require("../models/User");
+//const bcrypt = require('bcrypt')
+const User = require('../models/User')
 
 module.exports = {
-  async store(req, res) {
-    try {
-      const { email, firstName, lastName, password } = req.body;
-      const existentUser = await User.findOne({ email });
+	async store(req, res) {
+		try {
+			const { email, firstName, lastName, password } = req.body
+			const existentUser = await User.findOne({ email })
 
-      if (!existentUser) {
-        const user = await User.create({
-          email: email,
-          firstName: firstName,
-          lastName: lastName,
-          password: password,
-        });
-        return res.json(user);
-      }
-      return res.status(400).json({
-        message: "email already exist! do you want to login instead? ",
-      });
-    } catch (err) {
-      throw Error(`Error while Registering new user : ${err}`);
-    }
-  },
-};
+			if (!existentUser) {
+				//const hashedPassword = await bcrypt.hash(password, 10)
+				const user = await User.create({
+					email,
+					firstName,
+					lastName,
+          password,
+					//password: hashedPassword,
+				})
+				return res.json(user)
+			}
+			return res.status(400).json({
+				message:
+          'email already exist!  do you want to login instead? ',
+			})
+		} catch (err) {
+			throw Error(`Error while Registering new user :  ${err}`)
+		}
+	},
+}
