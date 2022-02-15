8082f635c03af51ac3626653756e938544ab46ed
MuhandOsman <mo1981ot@gmail.com>
2022-02-15 19:54:28 +0100

    added database file/ add unique email in usr schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const express = require('express')
-const mongoose = require('mongoose')
+require('./lib/database')
 const UserController = require('./controllers/UserController')
 const cors = require('cors')
 
@@ -9,7 +10,6 @@ const PORT = process.env.PORT || 8000
 app.use(cors())
 app.use(express.json())
 
-require('dotenv').config()
 
 app.get('/', (req, res) => {
     //we need to delete the line below after testing
@@ -25,27 +25,6 @@ app.get('/register', (req, res) => {
 
 app.post('/register', UserController.store)
 
-try {
-	mongoose.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	console.log('MongoDb connected successfully!')
-} catch (error) {
-	console.log(error)
-}
-
-// another way to connectting to mongoDB 
-// mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     }).then(() => {
-//         console.log('Connected to database successfully')
-//     })
-// .catch( (err) => {
-//     console.error(err)
-//     process.exit(1)
-// })
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`)
