abc8187ce55eeec531426dd8459da1485d092a55
MuhandOsman <mo1981ot@gmail.com>
2022-02-17 15:16:23 +0100

    add token to loginController and seting cookies

--- backend/src/controllers/LoginController.js
+++ backend/src/controllers/LoginController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/User');
+const {signToken} = require('../lib/token')
 
 module.exports = {
     async store(req, res) {
@@ -12,10 +13,10 @@ module.exports = {
 
             const user = await User.findOne({ email });
             if (!user) {
-                return res.status(200).json({ message: "User not found! Do you want to register instead?" })
+                return res.status(401).json({ message: "incorrect infos" })
             }
 
-            if (user && await bcrypt.compare(password, user.password)) {
+            /* if (user && await bcrypt.compare(password, user.password)) {
                 const userResponse = {
                     _id: user._id,
                     email: user.email,
@@ -24,9 +25,19 @@ module.exports = {
                 }
                 return res.json(userResponse)
             } else {
-                return res.status(200).json({ message: "Email or Password does not match!" })
+                return res.status(401).json({ message: "Email or Password does not match!" })
             }
+        */
+            const passwordCompare = await bcrypt.compare(password, user.password)
+            if (!passwordCompare) {
+                return res.status(401).json("could not login!")
+            } else {
 
+                const payload = {email : user.email , userId : user._id};
+                const token = signToken(payload);
+                res.cookie("jwt",token);
+                return res.status(200).json(token)
+            }
 
         } catch (error) {
             throw Error(`Error while Authenticating a User ${error}`)
