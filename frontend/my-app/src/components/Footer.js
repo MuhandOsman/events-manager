90e16739aa61dc664baba2f800215d0e45d64853
Jerreh <timeless_sound@hotmail.de>
2022-02-21 14:03:45 +0100

    react created ,the routes are linked ,everything suppose to work

---
+++ frontend/my-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+   
+      <h1>Made by @The Events Team 21.02.22</h1>
+    </div>
+  );
+}
+
+export default Footer
---
+++ frontend/my-app/src/components/Users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div>Users
+      <h1>Welcome To Users Page</h1>
+    </div>
+  )
+}
+
+export default Users
