e3935864c12a0e8fa0108c661ef0d78016bf60df
Jerreh <timeless_sound@hotmail.de>
2022-03-02 15:29:26 +0100

    footer modified

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import  {AiFillGithub}  from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer>
-   
       <div className="copyright">Made by @The Events Team 21.02.22</div>
+
+      <div className="footer-icons">
+        <AiFillGithub />
+        <AiOutlineInstagram />
+      </div>
     </footer>
   );
 }
