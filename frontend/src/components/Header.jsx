e728161515d989f26b1118bfa4cb85e827f87611
MuhandOsman <mo1981ot@gmail.com>
2022-03-03 21:37:36 +0100

    add nav strap & style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from './NavBar/NavBar'
 const Header = () => {
   return (
     <header>
-    <div className="logo">here will the logo go</div>
+    
         <Navbar/>
         
     </header>
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,15 +1,57 @@
-import Navigation from "./Navigation";
-import MobileNavigation from "./MobileNavigation"
+import { useState } from "react";
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
 
-import './NavBar.css'
+import "./NavBar.css";
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
-        <Navigation className="navigation"/>
-        <MobileNavigation/>
+      <div>
+        <Navbar className="navbar" expand="md" fixed="top" light>
+          <NavbarBrand href="/">LOGO</NavbarBrand>
+          <NavbarToggler
+            onClick={function toggler() {
+              setOpen(!open);
+            }}
+          />
+          <Collapse navbar isOpen={open}>
+            <Nav className="me-auto flex-nav" navbar>
+              <NavItem>
+                <NavLink href="/form/">Login/Register</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/about/">About us</NavLink>
+              </NavItem>
+              <UncontrolledDropdown inNavbar nav>
+                <DropdownToggle caret nav>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
