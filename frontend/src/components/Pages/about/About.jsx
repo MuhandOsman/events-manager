05e7ba874cd86ef7bd270c49e2b7b8935a4452f5
Gabriele Beretti <gabri.beretti@gmail.com>
2022-03-21 10:56:05 +0100

    title website

--- frontend/src/components/Pages/about/About.jsx
+++ frontend/src/components/Pages/about/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <section>
-      <h1>Welcome To The Events Page</h1>
+      <h1>About this project</h1>
+      <p>EVENTLIT help and connect people trough local events. Register a new profile and start immediately posting your events. People will be able to attend the events and you will be able to see how many attendings and tickets being sold on your event. Join our community now! </p>
     </section>
   );
 }
