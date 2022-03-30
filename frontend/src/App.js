31b76a0867219ec32317391fb7daf14864658b3b
Thomas Hofmann <thomas@savoy-truffle.de>
2022-03-30 12:44:10 +0200

    fix: file upload config in multer; and serving off frontend via /app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
-import MyProvider from "./context/MyProvider"
-import UsersProfile from "./components/Pages/user/UsersProfile"
-import Login from './components/Pages/login/Login';
-import About from './components/Pages/about/About';
-import Home from './components/Pages/home/Home';
-import EventDetail from "./components/Pages/eventDetail/EventDetail"
-import NewEvent from "./components/Pages/newEvent/NewEvent"
-import NotFound from './components/Pages/NotFound'
 import Footer from './components/Footer';
 import Header from "./components/Header";
+import About from './components/Pages/about/About';
+import EventDetail from "./components/Pages/eventDetail/EventDetail";
+import Home from './components/Pages/home/Home';
+import Login from './components/Pages/login/Login';
+import NewEvent from "./components/Pages/newEvent/NewEvent";
+import NotFound from './components/Pages/NotFound';
+import UsersProfile from "./components/Pages/user/UsersProfile";
+import MyProvider from "./context/MyProvider";
 
 function App() {
   return (
     <MyProvider >
-      <Router  >
+      <Router basename="/app">
         <Header />
       <Routes  >
         <Route path="/" element={<Home/>} />
